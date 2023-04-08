9202d895032e73c38dfef1d92218af844aeca936
Jose Elias Ribeiro Moreira <jose.moreira-sys@parceirosedu.com.br>
2023-04-08 14:21:36 -0300

    remove security group

--- aws-ec2/main.tf
+++ aws-ec2/main.tf
@@ -30,33 +30,3 @@
 
     tags = "${merge (var.ec2_tags) }"
   }
-
-resource "aws_security_group" "allow_http_https" {
-  name          = "allow_http_https"
-  description   = "Allow HTTP and HTTPS from Anywhere"
-  vpc_id        = var.vpc_id
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "allow_http_https"
-  }
-}
