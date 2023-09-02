f435eb33b25113798f0b511f5e81695c67dbefea
Jose Elias Ribeiro Moreira <jose.moreira-sys@parceirosedu.com.br>
2023-09-02 13:22:30 -0300

    fix

--- aws-s3-bucket/main.tf
+++ aws-s3-bucket/main.tf
@@ -51,8 +51,3 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
     }
   }
 }
-
-resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
-  bucket = aws_s3_bucket.example.id
-  policy = data.aws_iam_policy_document.allow_access_from_another_account.json
-}
