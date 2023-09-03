f7fb3f37d36a2cb1acdf588daabf54612acf2683
Jose Elias Ribeiro Moreira <jose.moreira-sys@parceirosedu.com.br>
2023-09-03 13:38:37 -0300

    fiz

--- aws-cdn/data.tf
+++ aws-cdn/data.tf
@@ -3,5 +3,5 @@ data "aws_caller_identity" "current" {}
 
 data "aws_route53_zone" "this" {
   name         = "${local.route53_base_domain}."
-  private_zone = local.route53_private_zone
+  private_zone = local.route53_base_domain
 }
