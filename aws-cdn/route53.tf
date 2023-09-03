9020351ce1bc4d5b6372322b8eb68cf9c1f1b4da
Jose Elias Ribeiro Moreira <jose.moreira-sys@parceirosedu.com.br>
2023-09-02 23:11:49 -0300

    add route53

--- aws-cdn/route53.tf
+++ aws-cdn/route53.tf
@@ -1,22 +1,27 @@
+resource "aws_route53_zone" "this" {
+  name = "automaticlabs.tech"
+}
+
 # data "aws_route53_zone" "this" {
 #   name         = "${local.route53_base_domain}."
 #   private_zone = local.route53_private_zone
 # }
 
+# Create Route53 Record to CloudFront
+resource "aws_route53_record" "this" {
+  name    = "automaticlabs.tech"
+  type    = "A"
+  zone_id = aws_route53_zone.this.zone_id
+  #zone_id = data.aws_route53_zone.domain_zone.zone_id
+  #name    = local.cdn_domain != "" ? local.cdn_domain : data.aws_route53_zone.domain_zone.name
+  
+  alias {
+    name                   = aws_cloudfront_distribution.this.domain_name
+    zone_id                = aws_cloudfront_distribution.this.hosted_zone_id
+    evaluate_target_health = false
+  }
 
-# # Create Route53 Record to CloudFront
-# resource "aws_route53_record" "this" {
-#   name    = local.cdn_domain != "" ? local.cdn_domain : data.aws_route53_zone.domain_zone.name
-#   type    = "A"
-#   zone_id = data.aws_route53_zone.domain_zone.zone_id
-
-#   alias {
-#     name                   = aws_cloudfront_distribution.cloudfront.domain_name
-#     zone_id                = aws_cloudfront_distribution.cloudfront.hosted_zone_id
-#     evaluate_target_health = false
-#   }
-
-#   depends_on = [
-#     aws_cloudfront_distribution.cloudfront
-#   ]
-# }
+  depends_on = [
+    aws_cloudfront_distribution.this
+  ]
+}
