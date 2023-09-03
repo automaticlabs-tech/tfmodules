b8d8be043f1c6fcb087a6562b5399cd7a7e4e96b
Jose Elias Ribeiro Moreira <jose.moreira-sys@parceirosedu.com.br>
2023-09-03 16:00:16 -0300

    add

--- aws-cdn/locals.tf
+++ aws-cdn/locals.tf
@@ -1,5 +1,5 @@
 locals {
-  s3_origin_id         = "automaticlabs.tech"
+  s3_origin_id         = var.route53_zone_domain
   cdn_domain           = var.cdn_domain
   route53_base_domain  = var.route53_zone_domain
 }
