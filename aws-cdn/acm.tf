11a32a1324cc0b7d5297edefa67f0dd6761fb3c1
Jose Elias Ribeiro Moreira <jose.moreira-sys@parceirosedu.com.br>
2023-09-02 23:54:11 -0300

    ix

--- aws-cdn/acm.tf
+++ aws-cdn/acm.tf
@@ -25,7 +25,7 @@ resource "aws_acm_certificate_validation" "certificate_validation" {
 # Add Certificate Validation Records on Route53
 resource "aws_route53_record" "certificate_records" {
   for_each = {
-    for dvo in aws_acm_certificate.this.domain_validation_options : dvo.domain_name => {
+    for dvo in aws_acm_certificate.certificate.domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
       record = dvo.resource_record_value
       type   = dvo.resource_record_type
